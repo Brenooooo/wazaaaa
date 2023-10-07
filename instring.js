ae1c924b7a37905d36c65fc3d39b85da6083866a
Brenooooo <140464191+Brenooooo@users.noreply.github.com>
2023-10-07 14:16:14 -0300

    Add files via upload

---
+++ instring.js
@@ -0,0 +1,15 @@
+console.log('hello world')
+
+let banana ='ovo';
+console.log(banana);
+
+banana = banana +' frito'
+console.log(banana);
+
+banana=banana.concat(' é bão')
+console.log(banana);
+
+const frase ='eu sou o batima'
+console.log(frase);
+
+console.log(frase.replace('batima', 'Bruxe wheine'))
